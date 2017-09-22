86294a41767de779f67c6bced1c3a66c1ed0ab62
Campbell Vertesi <campbell@vertesi.com>
2017-09-22 13:50:52 +0200

    String behavior for link fields

--- src/Drupal/Driver/Fields/Drupal8/LinkHandler.php
+++ src/Drupal/Driver/Fields/Drupal8/LinkHandler.php
@@ -13,12 +13,22 @@ class LinkHandler extends AbstractHandler {
   public function expand($values) {
     $return = array();
     foreach ($values as $value) {
+      // 'options' is required to be an array, otherwise the utility class
+      // Drupal\Core\Utility\UnroutedUrlAssembler::assemble() will complain.
+      $options = array();
+      $title = NULL;
+      $uri = $value;
+      if (is_array($value)) {
+        $title = $value[0];
+        $uri = $value[1];
+        if (!empty($value[2])) {
+          parse_str($value[2], $options);
+        }
+      }
       $return[] = array(
-        // 'options' is required to be an array, otherwise the utility class
-        // Drupal\Core\Utility\UnroutedUrlAssembler::assemble() will complain.
-        'options' => array(),
-        'title' => $value[0],
-        'uri' => $value[1],
+        'options' => $options,
+        'title' => $title,
+        'uri' => $uri,
       );
     }
     return $return;
